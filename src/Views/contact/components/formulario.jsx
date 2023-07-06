adea3b5934325423c56d7f9ae5dd7a59b52777c2
Amiel Ferreyra <amiel.ferrari@gmail.com>
2023-07-06 19:45:31 -0300

    estilo pagina terminado

--- src/Views/contact/components/formulario.jsx
+++ src/Views/contact/components/formulario.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import {useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
+import contimg from '../../../components/imagenes/contact-image.png'
 
 export const Formulario = () => {
 
-    const { register, formState: {errors}, handleSubmit } = useForm()
+    const { register, formState: { errors }, handleSubmit } = useForm()
 
     const onSubmit = (data) => {
         console.log(data)
@@ -13,8 +14,8 @@ export const Formulario = () => {
         <section>
             <div className="contact-wrapper" id="contacto">
                 <div className="contact-form">
-                    <h1 className="titulo">Get in touch <br/>
-                    <span>We are hiring!</span></h1>
+                    <h1 className="titulo">Get in touch <br />
+                        <span>We are hiring!</span></h1>
                     <form onSubmit={handleSubmit(onSubmit)}>
                         <div>
                             <label for="Nombre">Name</label>
@@ -23,13 +24,13 @@ export const Formulario = () => {
                                 name='name'
                                 placeholder='Name'
                                 {...register("name", {
-                                required: true,
-                                maxLength: 40
-                                 })}
+                                    required: true,
+                                    maxLength: 40
+                                })}
                             />
                             {errors.name?.type === 'required' && <p>El campo name es requerido</p>}
                             {errors.name?.type === 'maxLength' && <p>El campo name debe tener menos de
-                            40 caracteres</p>}
+                                40 caracteres</p>}
                         </div>
 
                         <div className="separador">
@@ -39,13 +40,13 @@ export const Formulario = () => {
                                 name='email'
                                 placeholder='e-mail'
                                 {...register("email", {
-                                required: true,
-                                maxLength: 60
+                                    required: true,
+                                    maxLength: 60
                                 })}
                             />
                             {errors.email?.type === 'required' && <p>El campo e-mail es requerido</p>}
                             {errors.email?.type === 'maxLength' && <p>El campo e-mail debe tener menos de
-                            20 caracteres</p>}
+                                20 caracteres</p>}
                         </div>
                         <div className="separador">
                             <label for="Telefono">Phone</label>
@@ -54,13 +55,13 @@ export const Formulario = () => {
                                 name='phone'
                                 placeholder='phone'
                                 {...register("phone", {
-                                required: true,
-                                maxLength: 15
+                                    required: true,
+                                    maxLength: 15
                                 })}
                             />
                             {errors.phone?.type === 'required' && <p>El campo phone es requerido</p>}
                             {errors.phone?.type === 'maxLength' && <p>El campo phone debe tener menos de
-                            12 caracteres</p>}
+                                12 caracteres</p>}
                         </div>
                         <div className="separador-mensaje">
                             <label for="Mensaje">Message</label>
@@ -69,23 +70,24 @@ export const Formulario = () => {
                                 name='message'
                                 placeholder='message'
                                 {...register("message", {
-                                required: true,
-                                maxLength: 500
+                                    required: true,
+                                    maxLength: 500
                                 })}
                             />
                             {errors.message?.type === 'required' && <p>El campo message es requerido</p>}
                             {errors.message?.type === 'maxLength' && <p>El campo message debe tener menos de
-                            100 caracteres</p>}
+                                100 caracteres</p>}
                         </div>
-                        <div>
-                            <button class="btn">Send</button>
+                        <div className="boton">
+                            <button className="botoncito">Send</button>
                         </div>
                     </form>
                 </div>
+                <div>
+                    <img src={contimg} alt="" />
+                </div>
             </div>
-            <div>
-                <img className="image-form" src="" alt=""/>
-            </div>
+
 
         </section>
     )
--- src/Views/home/components/NavBar.jsx
+++ src/Views/home/components/NavBar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './home.scss'
+import logo from '../../../components/imagenes/top-logo.png'
 
 function NavBar(){
     return(
-        <header className='head'>
+        <header className="head">
             <div className="logo">
-                <a href="/"><img src="../../../components/imagenes/top-logo.png" alt="logo" /></a>
+            <a href=""><img class="imglogo" src={logo} alt="logo"/></a>
             </div>
             <nav className="navbar">
                 <a href="#inicio">Home</a>
