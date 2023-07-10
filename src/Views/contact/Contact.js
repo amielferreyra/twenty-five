13bcd6ee2c7c462b77b55e2128501e230c3595ef
Amiel Ferreyra <amiel.ferrari@gmail.com>
2023-07-09 22:18:29 -0300

    formulario validado

--- src/Views/contact/Contact.js
+++ src/Views/contact/Contact.js
@@ -1,13 +1,47 @@
 import React from 'react'
-import { Formulario } from './components/formulario'
+import  Formulario  from './components/formulario'
 import './components/contact.scss'
+import axios from 'axios'
+import { useState } from "react"
 
-function Contact(){
-    return(
-        <section>
-            <Formulario/>
-        </section>
-    );
+function Contact() {
+    const [newUser, setNewUser] = useState({ name: "", phone: "", email: "", message: "", notifications: false })
+    const [success, setSuccess] = useState(false)
+    const [error, setError] = useState(false)
+    const [errorMessage, setErrorMessage] = useState("Something went wrong! Try again")
+
+
+    const disableSubmit = !newUser.name || !newUser.phone || !newUser.email || !newUser.message 
+
+
+    const handleChange = (event) => {
+        const property = event.target.id
+        const value = event.target.value
+        setNewUser({ ...newUser, [property]: value })
+        if (success) setSuccess(false)
+    }
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        axios.post('https://jsonplaceholder.typicode.com/posts', newUser)
+        .then(
+            response => {
+                setSuccess(true)
+                setNewUser({ name: "", phone: "", email: "", message: "", notifications: false })
+            }
+        ).catch(
+            error => {
+                setError(true)
+                error.message ? setErrorMessage(error.message) : setErrorMessage("Something went wrong! Try again")
+            }
+        )
+    }
+
+    return (
+        <Formulario newUser={newUser} errorMessage={errorMessage} error={error} success={success} disableSubmit={disableSubmit} handleChange={handleChange} handleSubmit={handleSubmit} />
+    )
 }
 
 export default Contact;
--- src/Views/contact/components/formulario.jsx
+++ src/Views/contact/components/formulario.jsx
@@ -1,97 +1,93 @@
 import React from 'react'
-import { useForm } from "react-hook-form";
 import contimg from '../../../components/imagenes/contact-image.png'
+import './contact.scss'
 
-export const Formulario = () => {
-
-    const { register, formState: { errors }, handleSubmit } = useForm()
-
-    const onSubmit = (data) => {
-        console.log(data)
-    }
-
+function formulario(props) {
     return (
-        <section>
-            <div className="contact-wrapper" id="contacto">
-                <div className="contact-form">
-                    <h1 className="titulo">Get in touch <br />
-                        <span>We are hiring!</span></h1>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <div>
-                            <label for="Nombre">Name</label>
-                            <input
-                                type="text"
-                                name='name'
-                                placeholder='Name'
-                                {...register("name", {
-                                    required: true,
-                                    maxLength: 40
-                                })}
-                            />
-                            {errors.name?.type === 'required' && <p>El campo name es requerido</p>}
-                            {errors.name?.type === 'maxLength' && <p>El campo name debe tener menos de
-                                40 caracteres</p>}
-                        </div>
+      <section>
+        <div className="contact-wrapper" id="contacto">
+          <div className="contact-form">
+            <h1 className="titulo">Get in touch <br />
+              <span>We are hiring!</span></h1>
+            <form onSubmit={props.handleSubmit}>
+              <div>
+                <label htmlFor="name">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  placeholder="Name"
+                  maxLength="40"
+                  onChange={props.handleChange}
+                  value={props.newUser.name}
+  
+                />
+              </div>
+              <div className="separador">
+                <label htmlFor="email">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="e-mail"
+                  maxLenght="60"
+                  aria-describedby="emailHelp"
+                  onChange={props.handleChange}
+                  value={props.newUser.email}
+                />
+              </div>
+              <div className="separador">
+                <label htmlFor="phone">
+                  Phone
+                </label>
+                <input
+                  type="tel"
+                  id="phone"
+                  placeholder="Phone number"
+                  maxLenght="12"
+                  onChange={props.handleChange}
+                  value={props.newUser.phone}
+                />
+              </div>
+              <div className="separador-mensaje">
+                <label htmlFor="message">
+                  Message
+                </label>
+                <input
+                  type="text-area"
+                  id="message"
+                  placeholder="Please, leave your message"
+                  maxLenght="500"
+                  onChange={props.handleChange}
+                  value={props.newUser.message}
+                />
+              </div>
+              <div className="boton">
+                <button disabled={props.disableSubmit} type="submit" className="botoncito">
+                  Send
+                </button>
+              </div>
+              {props.success &&
+                <div className="mensaje-exito" role="alert">
+                  Thank you! Your information has been received!
+                </div>}
+              {props.error &&
+                <div className="mensaje-error" role="alert">
+                  {props.errorMessage}
+                </div>}
+            </form>
+          </div>
+  
+          <div>
+            <img src={contimg} alt="" />
+          </div>
+        </div>
+      </section>
+  
+    );
+  }
 
-                        <div className="separador">
-                            <label for="Email">Email</label>
-                            <input
-                                type="email"
-                                name='email'
-                                placeholder='e-mail'
-                                {...register("email", {
-                                    required: true,
-                                    maxLength: 60
-                                })}
-                            />
-                            {errors.email?.type === 'required' && <p>El campo e-mail es requerido</p>}
-                            {errors.email?.type === 'maxLength' && <p>El campo e-mail debe tener menos de
-                                20 caracteres</p>}
-                        </div>
-                        <div className="separador">
-                            <label for="Telefono">Phone</label>
-                            <input
-                                type="text"
-                                name='phone'
-                                placeholder='phone'
-                                {...register("phone", {
-                                    required: true,
-                                    maxLength: 15
-                                })}
-                            />
-                            {errors.phone?.type === 'required' && <p>El campo phone es requerido</p>}
-                            {errors.phone?.type === 'maxLength' && <p>El campo phone debe tener menos de
-                                12 caracteres</p>}
-                        </div>
-                        <div className="separador-mensaje">
-                            <label for="Mensaje">Message</label>
-                            <input
-                                type="text-area"
-                                name='message'
-                                placeholder='message'
-                                {...register("message", {
-                                    required: true,
-                                    maxLength: 500
-                                })}
-                            />
-                            {errors.message?.type === 'required' && <p>El campo message es requerido</p>}
-                            {errors.message?.type === 'maxLength' && <p>El campo message debe tener menos de
-                                100 caracteres</p>}
-                        </div>
-                        <div className="boton">
-                            <button className="botoncito">Send</button>
-                        </div>
-                    </form>
-                </div>
-                <div>
-                    <img src={contimg} alt="" />
-                </div>
-            </div>
-
-
-        </section>
-    )
-}
-
-
+export default formulario;
 
